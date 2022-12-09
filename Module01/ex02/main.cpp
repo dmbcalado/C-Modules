4fb2f6ec0f1a4ef7c3c7e8df03faff61e3a9b580
RatinhoSujo <dmendonc>
2022-12-09 03:50:47 +0000

    done for today

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <string>
+# include <cstring>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string const &stringREF = (std::string const&)str;
+
+	std::cout << "Memory address of the string variable:" << std::endl; 
+	std::cout << &str << std::endl;
+	std::cout << "Memory address held by stringPTR:" << std::endl; 
+	std::cout << &stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF:" << std::endl; 
+	std::cout << &stringREF << std::endl << std::endl;
+	std::cout << "Value of the string variable:" << std::endl; 
+	std::cout << str << std::endl;
+	std::cout << "Value pointed to by stringPTR:" << std::endl; 
+	std::cout << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF:" << std::endl; 
+	std::cout << stringREF << std::endl << std::endl;
+}
